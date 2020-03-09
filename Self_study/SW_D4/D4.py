25d268b59ae5fd3367604047df5fd7e3dcea7bc4
XECOH <hesnying@gmail.com>
2020-03-09 22:07:03 +0900

    Add .

--- Self_study/SW_D4/D4.py
+++ Self_study/SW_D4/D4.py
@@ -167,15 +167,23 @@
 
 # 4408
 
-for tc in range(1, int(input())+1):
-    n = int(input())
-    rooms = [0] * 401
-    for i in range(n):
-        cr, mr = map(int, input().split())
-        for c in range(cr+1, mr+1):
-            rooms[c] += 1
-    print('#{} {}'.format(tc, max(rooms)))
-    
+# for tc in range(1, int(input())+1):
+#     n = int(input())
+#     rooms = [0] * 200
+#     for i in range(n):
+#         cr, mr = map(int, input().split())
+#         if cr % 2:
+#             cr = (cr//2)
+#         else:
+#             cr = (cr//2)-1
+#         if mr % 2:
+#             mr = (mr//2)
+#         else:
+#             mr = (mr//2)-1
+#         for c in range(min(cr, mr), max(cr, mr)+1):
+#             rooms[c] += 1
+#     print('#{} {}'.format(tc, max(rooms)))
+
 
 # 3347
 
