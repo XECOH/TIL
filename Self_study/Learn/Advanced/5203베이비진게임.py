15d5769a7f5b2820f081e72857e20406bd35b37f
XECOH <hesnying@khu.ac.kr>
2020-05-06 16:44:03 +0900

    babyjin

--- Self_study/Learn/Advanced/5203베이비진게임.py
+++ Self_study/Learn/Advanced/5203베이비진게임.py
@@ -1,4 +1,5 @@
 def fjs(player):
+    player = list(set(player))
     l = len(player)
     cnt = 0
     for i in range(l-1):
@@ -10,28 +11,29 @@ def fjs(player):
             return 1
     return 0
 
-def babyjin(player):
+def triplet(player):
     l = len(player)
-    i = 0
-    while i < l:
+    for i in range(l):
         if player.count(player[i]) >= 3:
             return 1
-        else:
-            i += 1
     return 0
 
 for tc in range(1, int(input())+1):
+    ans = 0
     cards = list(map(int, input().split()))
     i = 3
     while i < 7:
         p1 = sorted([cards[j] for j in range(0, i*2, 2)])
-        p2 = sorted([cards[j] for j in range(1, i*2, 2)])
         r1 = fjs(p1)
-        b1 = babyjin(p1)
+        b1 = triplet(p1)
+        if r1 != 0 or b1 != 0:
+            ans = 1
+            break
+        p2 = sorted([cards[j] for j in range(1, i*2, 2)])
         r2 = fjs(p2)
-        b2 = babyjin(p2)
-
+        b2 = triplet(p2)
+        if r2 != 0 or b2 != 0:
+            ans = 2
+            break
         i += 1
-
-    if i == 7:
-        print('#{} {}'.format(tc, 0))
+    print('#{} {}'.format(tc, ans))
