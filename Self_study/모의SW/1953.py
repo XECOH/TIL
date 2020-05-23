983342d7d90dc4fc65e0baa56d3b93355a02d0f8
XECOH <hesnying@khu.ac.kr>
2020-05-23 13:39:48 +0900

    탈주범 검거

--- Self_study/모의SW/1953.py
+++ Self_study/모의SW/1953.py
@@ -1,69 +1,77 @@
-def find(R, C, t, L):
-    global ans
+from collections import deque
+
+def find(t, L, cnt):
+    Q = deque()
+    Q.append((r, c, 1))
     dR = [-1, 0, 1, 0]
     dC = [0, 1, 0, -1]
-    if t == L: return
-    if visited[R][C] != 0: return
-    else:
-        visited[R][C] = 1
+    x = [[0, 3, 4, 7], [0, 2, 4, 5], [0, 3, 5, 6], [0, 2, 6, 7]]
+    while Q:
+        if t == L:
+            break
+        R, C, T = Q.popleft()
         if dungeon[R][C] == 1:
             for k in range(4):
                 nR, nC = R + dR[k], C + dC[k]
-                if nR < 0 or nR >= m or nC < 0 or nC >= n: continue
-                if dungeon[nR][nC] != 0 and visited[nR][nC] == 0:
-                    ans += 1
-                    find(nR, nC, t+1, L)
+                if nR < 0 or nR >= n or nC < 0 or nC >= m: continue
+                if dungeon[nR][nC] not in x[k] and visited[nR][nC] == 0 and T+1 <= L:
+                    visited[nR][nC] = 1
+                    Q.append((nR, nC, T+1))
+                    cnt += 1
         elif dungeon[R][C] == 2:
             for k in range(0, 4, 2):
                 nR, nC = R + dR[k], C + dC[k]
-                if nR < 0 or nR >= m or nC < 0 or nC >= n: continue
-                if dungeon[nR][nC] != 0 and visited[nR][nC] == 0:
-                    ans += 1
-                    find(nR, nC, t+1, L)
+                if nR < 0 or nR >= n or nC < 0 or nC >= m: continue
+                if dungeon[nR][nC] not in x[k] and visited[nR][nC] == 0 and T+1 <= L:
+                    visited[nR][nC] = 1
+                    Q.append((nR, nC, T+1))
+                    cnt += 1
         elif dungeon[R][C] == 3:
             for k in range(1, 4, 2):
                 nR, nC = R + dR[k], C + dC[k]
-                if nR < 0 or nR >= m or nC < 0 or nC >= n: continue
-                if dungeon[nR][nC] != 0 and visited[nR][nC] == 0:
-                    ans += 1
-                    find(nR, nC, t+1, L)
+                if nR < 0 or nR >= n or nC < 0 or nC >= m: continue
+                if dungeon[nR][nC] not in x[k] and visited[nR][nC] == 0 and T+1 <= L:
+                    visited[nR][nC] = 1
+                    Q.append((nR, nC, T+1))
+                    cnt += 1
         elif dungeon[R][C] == 4:
-            for k in range(1, 3):
+            for k in range(0, 2):
                 nR, nC = R + dR[k], C + dC[k]
-                if nR < 0 or nR >= m or nC < 0 or nC >= n: continue
-                if dungeon[nR][nC] != 0 and visited[nR][nC] == 0:
-                    ans += 1
-                    find(nR, nC, t+1, L)
+                if nR < 0 or nR >= n or nC < 0 or nC >= m: continue
+                if dungeon[nR][nC] not in x[k] and visited[nR][nC] == 0 and T+1 <= L:
+                    visited[nR][nC] = 1
+                    Q.append((nR, nC, T+1))
+                    cnt += 1
         elif dungeon[R][C] == 5:
             for k in range(1, 3):
                 nR, nC = R + dR[k], C + dC[k]
-                if nR < 0 or nR >= m or nC < 0 or nC >= n: continue
-                if dungeon[nR][nC] != 0 and visited[nR][nC] == 0:
-                    ans += 1
-                    find(nR, nC, t+1, L)
+                if nR < 0 or nR >= n or nC < 0 or nC >= m: continue
+                if dungeon[nR][nC] not in x[k] and visited[nR][nC] == 0 and T+1 <= L:
+                    visited[nR][nC] = 1
+                    Q.append((nR, nC, T+1))
+                    cnt += 1
         elif dungeon[R][C] == 6:
             for k in range(2, 4):
                 nR, nC = R + dR[k], C + dC[k]
-                if nR < 0 or nR >= m or nC < 0 or nC >= n: continue
-                if dungeon[nR][nC] != 0 and visited[nR][nC] == 0:
-                    ans += 1
-                    find(nR, nC, t+1, L)
+                if nR < 0 or nR >= n or nC < 0 or nC >= m: continue
+                if dungeon[nR][nC] not in x[k] and visited[nR][nC] == 0 and T+1 <= L:
+                    visited[nR][nC] = 1
+                    Q.append((nR, nC, T+1))
+                    cnt += 1
         elif dungeon[R][C] == 7:
             for k in range(0, 4, 3):
                 nR, nC = R + dR[k], C + dC[k]
-                if nR < 0 or nR >= m or nC < 0 or nC >= n: continue
-                if dungeon[nR][nC] != 0 and visited[nR][nC] == 0:
-                    ans += 1
-                    find(nR, nC, t+1, L)
-    return
+                if nR < 0 or nR >= n or nC < 0 or nC >= m: continue
+                if dungeon[nR][nC] not in x[k] and visited[nR][nC] == 0 and T+1 <= L:
+                    visited[nR][nC] = 1
+                    Q.append((nR, nC, T+1))
+                    cnt += 1
+    return cnt
 
 for tc in range(1, int(input())+1):
-    n, m, r, c, l = map(int, input().split()) # 세로, 가로, 맨홀 뚜껑 세로, 맨홀 뚜껑 가로, 탈출 소요 시간
+    n, m, r, c, l = map(int, input().split())
     dungeon = [list(map(int, input().split())) for _ in range(n)]
     visited = [[0]*m for _ in range(n)]
-    ans = 1
-    if l == 1:
-        print('#{} {}'.format(tc, ans))
-    else:
-        find(r, c, 1, l)
-        print('#{} {}'.format(tc, ans))
+    visited[r][c] = 1
+    ans = find(1, l, 1)
+    print('#{} {}'.format(tc, ans))
